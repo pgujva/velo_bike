697e8aaf6b698e0108cfd327eaa4f044e66bc122
pgujva <pgujva@usetech.ru>
2018-08-15 14:50:03 +0300

    Создан проект велобайк

--- Velobike/src/main/java/MyFirstProgram.java
+++ Velobike/src/main/java/MyFirstProgram.java
@@ -1,30 +1,9 @@
-package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Pasha");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(4,6);
-    r.a = 4;
-    r.b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-  }
-
-  public static void hello(String somebody) {
-
-    System.out.println("Hello, " + somebody + "!");
-  }
-
-
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
+    System.out.println("Hello World");
   }
 
 }
