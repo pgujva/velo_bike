ed75f558f6167c4eedbbfc77605f084d42969886
pgujva <pgujva@usetech.ru>
2018-08-24 19:07:21 +0300

    55656

--- Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/appManager/HttpSession.java
+++ Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/appManager/HttpSession.java
@@ -1,10 +1,17 @@
 package ru.usetech.pft.velobike.appManager;
 
-public class HttpSession {
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 
+import java.io.Closeable;
+
+public class HttpSession {
+    private CloseableHttpClient httpclient;
     private ApplicationManager app;
 
     public HttpSession(ApplicationManager app) {
         this.app = app;
+        httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
     }
 }
