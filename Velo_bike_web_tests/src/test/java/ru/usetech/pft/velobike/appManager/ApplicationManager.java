fa513134c29ac5f696e9c3ddff44611860fe2029
pgujva <pgujva@usetech.ru>
2018-11-01 18:26:41 +0300

    исправление ошибок

--- Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/appManager/ApplicationManager.java
+++ Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/appManager/ApplicationManager.java
@@ -9,78 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-<<<<<<< HEAD
-    ChromeDriver wd;
-    private NavigationHelper navigationHelper;
-    private SessionHelper sessionHelper;
-    private EnterToSystemHelper enterToSystemHelper;
-    private HelperBase helperBase;
-    private SideMenuHelper sideMenuHelper;
-    private NewsPageHelper newsPageHelper;
-    private PricePageHelper pricePageHelper;
-    private ElectroVoloBikeHelper electroVoloBikeHelper;
-    private WebDriverWait wait;
 
-    public void init() {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        wait = new WebDriverWait(wd, 20);
-        //wd.manage().window().fullscreen();
-        wd.get("https://velobike.ru/");
-        helperBase = new HelperBase(wd, wait);
-        navigationHelper = new NavigationHelper(wd, wait);
-        sessionHelper = new SessionHelper(wd, wait);
-        enterToSystemHelper = new EnterToSystemHelper(wd, wait);
-        sideMenuHelper = new SideMenuHelper(wd, wait);
-        newsPageHelper = new NewsPageHelper(wd, wait);
-        pricePageHelper = new PricePageHelper(wd,wait);
-        electroVoloBikeHelper = new ElectroVoloBikeHelper(wd,wait);
-        sessionHelper.loginInSideMenu("4001776", "3875");
-    }
-
-    public void stop() {
-     wd.quit();
-    }
-
-    public HttpSession newSession() {
-        return new HttpSession(this);
-    }
-
-
-
-
-    public NavigationHelper getNavigationHelper() {
-        return navigationHelper;
-    }
-
-    public EnterToSystemHelper getEnterToSystemHelper() {
-        return enterToSystemHelper;
-    }
-
-    public SessionHelper getSessionHelper() {
-        return sessionHelper;
-    }
-
-    public HelperBase getHelperBase() {
-        return helperBase;
-    }
-
-    public SideMenuHelper getSideMenuHelper() {
-        return sideMenuHelper;
-    }
-
-    public NewsPageHelper getNewsPageHelper() {
-        return newsPageHelper;
-    }
-
-    public PricePageHelper getPricePageHelper() {
-        return pricePageHelper;
-    }
-
-    public ElectroVoloBikeHelper getElectroVoloBikeHelper() {
-        return electroVoloBikeHelper;
-    }
-=======
   ChromeDriver wd;
   private NavigationHelper navigationHelper;
   private SessionHelper sessionHelper;
@@ -161,6 +90,6 @@ public class ApplicationManager {
   public MainPageHelper getMainPageHelper () {
     return mainPageHelper;
   }
->>>>>>> c2e583ff4fd319541f15864ff2ec9251a9437e03
+
 }
 
