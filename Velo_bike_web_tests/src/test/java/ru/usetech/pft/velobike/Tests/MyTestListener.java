0a53abe34aff120e696f34745f2c8e33a4c97c04
pgujva <pgujva@usetech.ru>
2019-01-16 13:47:31 +0300

    43434

--- Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/Tests/MyTestListener.java
+++ Velo_bike_web_tests/src/test/java/ru/usetech/pft/velobike/Tests/MyTestListener.java
@@ -10,55 +10,52 @@ import java.io.File;
 import java.io.IOException;
 
 public class MyTestListener implements ITestListener {
-    @Override
-    public void onTestStart(ITestResult result) {
+  @Override
+  public void onTestStart(ITestResult result) {
 
-    }
+  }
 
-    @Override
-    public void onTestSuccess(ITestResult result) {
+  @Override
+  public void onTestSuccess(ITestResult result) {
 
-    }
+  }
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-        ApplicationManager app = (ApplicationManager) result.getTestContext().getAttribute("app");
-        try {
-            saveScreenshot(app.takeScreenshot());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    @Attachment(value = "Page screenshot", type = "image/png")
-
-    public File saveScreenshot(File screenShot) {
-        return screenShot;
+  @Override
+  public void onTestFailure(ITestResult result) {
+    ApplicationManager app = (ApplicationManager) result.getTestContext().getAttribute("app");
+    try {
+      saveScreenshot(app.takeScreenshot());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
 
+  }
+
+  @Attachment(value = "Page screenshot", type = "image/png")
+
+  public File saveScreenshot(File screenShot) {
+    return screenShot;
+  }
 
 
+  @Override
+  public void onTestSkipped(ITestResult result) {
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
+  }
 
-    }
+  @Override
+  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+  }
 
-    }
+  @Override
+  public void onStart(ITestContext context) {
 
-    @Override
-    public void onStart(ITestContext context) {
+  }
 
-    }
+  @Override
+  public void onFinish(ITestContext context) {
 
-    @Override
-    public void onFinish(ITestContext context) {
-
-    }
+  }
 }
